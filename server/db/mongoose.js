8ba5cde8f494e012efa757ac3e1417d93f26f3cd
Yatseyko Yuriy <yura.volpis@gmail.com>
2018-11-23 19:33:32 +0200

    BD fixes

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,4 +4,7 @@ mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/WayaApp');
 // mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/WayaApp');
 
+mongoose.set('useCreateIndex', true)
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/WayaApp', { useNewUrlParser: true });
+
 module.exports = {mongoose};
